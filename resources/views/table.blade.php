f8e64195286d2b93e3eeef2aa53141b81e236d8a
youdipugo <kzryanarinzolojt@gmail.com>
2023-12-12 13:55:55 +0800

    table

---
+++ resources/views/table.blade.php
@@ -0,0 +1,88 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <button class="backbtn">Back</button>
+<div class="wholetbl">   
+<table class="table">
+<thead>
+<th>Customer</th>
+<th>Customer</th>
+<th>Customer</th>
+<th>Customer</th>
+
+</thead>
+<tbody>
+    <tr>
+        <td data-label="Customer"><input type="checkbox" id="check">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+          
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+             
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+    <tr>
+        <td data-label="Customer"><input type="checkbox">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+        <td data-label="Customer">customer</td>
+              
+    </tr>
+</tbody>
+</table>
+</div>   
+</body>
+</html>
